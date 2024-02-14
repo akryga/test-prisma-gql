7bf74f4a0f9e13b2206cf79b4af1d4539b16b280
akryga <akryga@gmail.com>
2024-02-14 09:31:35 +0300

    prisma and front example

--- server.ts
+++ server.ts
@@ -1,24 +1,53 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
+import "reflect-metadata";
 import express from 'express';
-import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import { createYoga, createSchema } from 'graphql-yoga';
+import { resolvers } from "./prisma/generated/type-graphql/index";
+import { buildSchema } from "type-graphql";
+import { PrismaClient } from "@prisma/client";
+
+
+import { fileURLToPath } from 'node:url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // The Express app is exported so that it can be used by serverless Functions.
-export function app(): express.Express {
+export async function app() {
+
+ 
+// Bind GraphQL Yoga to the graphql endpoint to avoid rendering the playground on any path
+
   const server = express();
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
   const indexHtml = join(serverDistFolder, 'index.server.html');
-
+  const prisma = new PrismaClient();
   const commonEngine = new CommonEngine();
 
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
+  const schema = await buildSchema({
+    resolvers,
+    validate: false,
+  });
 
+  //  const schema = makeExecutableSchema({ resolvers });
+
+  const yoga = createYoga({
+    schema, 
+    context: () => ({ prisma })
+  })
+  server.use(yoga.graphqlEndpoint, yoga)
+  
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
+
+  
   // Serve static files from /browser
   server.get('*.*', express.static(browserDistFolder, {
     maxAge: '1y'
@@ -40,17 +69,26 @@ export function app(): express.Express {
       .catch((err) => next(err));
   });
 
-  return server;
-}
-
-function run(): void {
+  
   const port = process.env['PORT'] || 4000;
-
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+    server.listen(port, () => {
+      console.log(`Node Express server listening on http://localhost:${port}`);
+    });
+  // return server;
 }
 
-run();
+app()
+  .catch(console.error)
+
+// function run(): void {
+//   const port = process.env['PORT'] || 4000;
+
+//   // Start up the Node server
+//   const server = app();
+//   server.listen(port, () => {
+//     console.log(`Node Express server listening on http://localhost:${port}`);
+//   });
+// }
+
+// run();
+
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css'
+})
+export class UsersComponent {
+  refresh() {
+    this.getData(encodeURI("/graphql"),{query:"{ users { id  name CREATED }}"}).then((data) => {
+  console.log(data); // JSON data parsed by `response.json()` call
+});
+
+  }
+  async getData(url = "", data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        //'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *client
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    return await response.json(); // parses JSON response into native JavaScript objects
+  }
+}
